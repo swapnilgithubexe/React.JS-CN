fccb5bcee6c370edf6e405549f7cf9125a2591e6
swapnilgithubexe <swapnildutta2002@gmail.com>
2025-02-04 17:32:49 +0530

    updates

---
+++ index.js
@@ -0,0 +1,6 @@
+
+const heading = document.createElement("div");
+heading.className = "header";
+heading.textContent = "This is a heading"
+const rootElement = document.getElementById("root")
+rootElement.appendChild(heading);
