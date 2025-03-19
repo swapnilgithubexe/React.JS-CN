e1d19ba340f6502464f2d22de4184f0d6ad5a099
swapnilgithubexe <swapnildutta2002@gmail.com>
2025-03-19 12:16:19 +0530

    shopping cart and color picking updates

---
+++ Context API/useContext Hook/src/Card.js
@@ -0,0 +1,17 @@
+import { useContext } from "react";
+import { cardContext } from "./context";
+//Refactor this to use the useContext hook
+export const Card = () => {
+  const value = useContext(cardContext);
+  return (
+    <cardContext.Provider value={value}>
+
+      return (
+      <div className="card" style={{ backgroundColor: value.color }}>
+        <h3>{value.text}</h3>
+      </div>
+      );
+
+    </cardContext.Provider>
+  );
+};
