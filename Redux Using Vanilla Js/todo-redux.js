228646741c6a1449942f153aa7f78495049acaae
swapnilgithubexe <swapnildutta2002@gmail.com>
2025-04-01 22:19:50 +0530

    started redux with vanilla js

---
+++ Redux Using Vanilla Js/todo-redux.js
@@ -0,0 +1,48 @@
+import redux from "redux";
+
+//Actions
+
+const ADD_TODO = "ADD TODO";
+const TOGGLE_TODO = "TOGGLE TODO";
+
+//Action creators
+
+const addToDo = (text) => ({ text, type: ADD_TODO });
+const toggleToDo = (index) => ({ index, type: TOGGLE_TODO })
+
+//Initial Data
+const initialState = {
+  todos: []
+}
+//Reducers
+
+//Reducers must return updated state
+const todoReducer = (state = initialState, action) => {
+  switch (action.type) {
+
+    case ADD_TODO:
+      return {
+        ...state,
+        todos: [
+          ...state.todos,
+          {
+            text: action.text,
+            completed: false
+          }
+        ]
+      }
+    case TOGGLE_TODO:
+      return {
+        ...state,
+        todos: todos.mao((todo, i) => {
+          if (i === action.index) {
+            todo.completed = !todo.completed
+          }
+          return todo
+        })
+      }
+    default:
+      return state;
+  }
+}
+
