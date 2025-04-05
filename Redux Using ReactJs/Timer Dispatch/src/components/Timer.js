9fc03dc9a764adbcc0a81d90f4668c0a8da2500a
swapnilgithubexe <swapnildutta2002@gmail.com>
2025-04-05 20:57:13 +0530

    Timer Dispatch

---
+++ Redux Using ReactJs/Timer Dispatch/src/components/Timer.js
@@ -0,0 +1,24 @@
+import { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { incrementTimer } from "../redux/actions/timerActions";
+
+export const Timer = () => {
+  const dispatch = useDispatch();
+  const { isRunning, elapsedTime } = useSelector((state) => state);
+
+  useEffect(() => {
+    let interval;
+    if (isRunning) {
+      interval = setInterval(() => {
+        // disptach incrementTimer action here
+        dispatch(incrementTimer())
+
+      }, 1000);
+    }
+    return () => {
+      clearInterval(interval);
+    };
+  }, [isRunning]);
+
+  return <b>{elapsedTime}</b>;
+};
