a9facb7b2a0e0c07990aceac7de7d731147b55ad
swapnilgithubexe <swapnildutta2002@gmail.com>
2025-04-10 23:34:00 +0530

    moved the utility app

--- Redux Using ReactJs/toDoUtility App/src/redux/actions/noteAction.js
+++
@@ -1,6 +0,0 @@
-export const ADD_NOTE = "Add Note"
-export const DELETE_NOTE = "Delete Node"
-
-export const addNote = (text) => ({ text, type: ADD_NOTE });
-
-export const deleteNote = (index) => ({ index, type: DELETE_NOTE });
